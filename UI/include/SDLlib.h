9f3a8132ec9b1a998aa88e244ef1bcf44ac01e6f
Djcharles26 <jlaraanaya@gmail.com>
2018-11-02 14:18:45 -0600

    Graphic Update 1.1
    Changed the way to Print things on window

--- UI/include/SDLlib.h
+++ UI/include/SDLlib.h
@@ -1,22 +1,19 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-int SDL_STARTER(SDL_Window **window,SDL_Surface **windowSurface, char message[100]);
-void SDL_PRINT(SDL_Surface **imageSurface, SDL_Surface **windowSurface, SDL_Window **window);
-void IMG_Loader(SDL_Surface **image, char PATH[100]);
-
-
 
 #define center  SDL_WINDOWPOS_CENTERED
-#define HEIGHT 600
-#define WEIGHT 600
+#define HEIGHT 450
+#define WIDTH 450
+#define pressedkey key.keysym.sym
 
-int SDL_STARTER(SDL_Window **window,SDL_Surface **windowSurface, char message[100]){
+int SDL_STARTER(SDL_Window **window,SDL_Renderer **renderer, char message[100]){
     /**
      * Initialize SDL, creates a window and asign a window surface
     */
-    *window = SDL_CreateWindow(message,center,center,WEIGHT,HEIGHT,SDL_WINDOW_SHOWN);
-    *windowSurface = SDL_GetWindowSurface(*window);//create a new window to renderer
+    *window = SDL_CreateWindow(message,center,center,WIDTH,HEIGHT,SDL_WINDOW_SHOWN);
+    *renderer = SDL_CreateRenderer(*window,-1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
+    SDL_StartTextInput();
     if(!*window){
         fprintf(stderr,"[ERR]ERROR CREATING WINDOW\n");
         return -1;
@@ -25,15 +22,67 @@ int SDL_STARTER(SDL_Window **window,SDL_Surface **windowSurface, char message[10
     }
 }
 
-void SDL_PRINT(SDL_Surface **imageSurface, SDL_Surface **windowSurface, SDL_Window **window){
-    /** 
-     * Prints an image in the surface and reloads the window
-    */
-    SDL_BlitSurface(*imageSurface,NULL,*windowSurface,NULL);//print image in the new renderer
-    SDL_UpdateWindowSurface(*window);//update the window
+void IMG_Loader(SDL_Surface **image, char PATH[100]){
+    
+    *image = IMG_Load(PATH);//loads an image
 }
 
-void IMG_Loader(SDL_Surface **image, char PATH[100]){
-    printf("%s",PATH);
-    *image = IMG_Load(PATH);//loads an image
+void SDL_exit(SDL_Event event, bool *exit){
+    while(SDL_PollEvent(&event) != 0){
+        if(event.type == SDL_QUIT){
+            *exit=true;
+        }else if(event.type == SDL_KEYDOWN){
+            switch(event.pressedkey){
+                case SDLK_ESCAPE:
+                    *exit = true;
+                break;
+            }
+        }
+    }
+}
+
+SDL_Rect createRect(int x, int y){
+    SDL_Rect rect;
+    rect.x = x;
+    rect.y = y;
+    rect.w = WIDTH;
+    rect.h = HEIGHT;
+    return rect;
+}
+
+SDL_Rect createRectFixed(int x,int y, int w,int h){
+    SDL_Rect rect;
+    rect.x = x;
+    rect.y = y;
+    rect.w = w;
+    rect.h = h;
+    return rect;
+}
+
+SDL_Surface *OptimizedSurface(char filepath[100],SDL_Surface *windowSurface){
+    SDL_Surface *optimizedSurface = NULL;
+    SDL_Surface *surface = NULL;
+    IMG_Loader(&surface,filepath);  
+    if(!surface){
+        printf("ERROR");
+    }else{
+        optimizedSurface = SDL_ConvertSurface(surface,windowSurface->format,0);
+    }
+    SDL_FreeSurface(surface);
+    return optimizedSurface;
+}
+
+SDL_Texture *LoadTexture(char filePath[100],SDL_Renderer *renderTarget){
+    SDL_Texture *texture = NULL;
+    SDL_Surface *surface = NULL;
+    IMG_Loader(&surface,filePath);
+    if(!surface){
+        printf("ERROR");
+    }
+    texture = SDL_CreateTextureFromSurface(renderTarget,surface);
+    if(!texture){
+        printf("ERROR TEXTURE, %s\n",SDL_GetError());
+    }   
+    SDL_FreeSurface(surface);
+    return texture;
 }
--- UI/src/main.c
+++ UI/src/main.c
@@ -4,39 +4,86 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdbool.h>
-#include "SDLlib.h"
 
+#include "SDLlib.h"
+#define dim 100
 #define pressedkey key.keysym.sym
 
 
+
+
+
 int main(void){
-    bool exit=false;
-    SDL_Window *window = NULL;
-    SDL_Event event;
-    SDL_Surface *imageSurface = NULL;
-    SDL_Surface *windowSurface = NULL;
-    if(SDL_STARTER(&window,&windowSurface,"cimage")==0){
-        windowSurface = SDL_GetWindowSurface(window);
-        IMG_Loader(&imageSurface,"./resources/pp.jpg");
+    SDL_Init(SDL_INIT_EVERYTHING);
+    char text[10];
+    bool exit = false, end = false;
+    SDL_Window *window = NULL;//principal window
+    SDL_Event event;//the event checker
+    SDL_Renderer *renderTarget;
+    SDL_Texture *texture[100];
+    SDL_Surface *example = NULL;
+    SDL_Rect drawingRect;//rectangle
+    drawingRect = createRectFixed(0,0,200,200);
+    //WE will use renderer for better results
+    if(SDL_STARTER(&window,&renderTarget,"cimage")==0){
+        texture[1] = LoadTexture("./resources/pp.jpg",renderTarget);
+        texture[2] = LoadTexture("./resources/Star_Wars.png",renderTarget);
+        SDL_SetRenderTarget(renderTarget,texture[0]);
+        SDL_SetRenderDrawColor(renderTarget,255,0,0,255);
+        SDL_Rect rectangle = createRectFixed(0,0,50,50);
+        SDL_RenderFillRect(renderTarget,&rectangle);
         while(!exit){
-            SDL_PRINT(&imageSurface,&windowSurface,&window);
-            while(SDL_PollEvent(&event)!=0){
+            while(SDL_PollEvent(&event)!=0){//EVENTS
                 if(event.type == SDL_QUIT){
                     exit=true;
-                }else if(event.type == SDL_KEYDOWN){
+                }else if(event.type == SDL_KEYDOWN){//exit out
                     switch(event.pressedkey){
-                        case SDLK_1:
+                        case SDLK_ESCAPE:
                             exit = true;
                         break;
-                        case SDLK_a:
-                            printf("Presionaste la A");
-                            break;
+                    }
+                }else if(event.type == SDL_MOUSEBUTTONDOWN){//mouse events
+                    switch(event.button.button){
+                        case SDL_BUTTON_LEFT:   
+                            // SDL_FreeSurface(windowSurface);
+                            // images[0] = images[2];
+                            SDL_RenderClear(renderTarget);
+                            texture[0]=texture[1];
+
+                        break;
+                        case SDL_BUTTON_RIGHT:
+                            // SDL_FreeSurface(windowSurface);
+                            // images[0] = images[1];
+                            SDL_RenderClear(renderTarget);
+                            texture[0]=texture[2];
+                        break;
+                    }
+                }else if(event.type == SDL_TEXTINPUT){
+                    if(!end){
+                        system("clear");
+                        strcpy(text,event.text.text);
+                        printf("%s",text);
+                    }
+                }else if(event.type == SDL_KEYDOWN && event.pressedkey  == SDLK_RETURN){
+                        end=true;
+                }else if(event.type == SDL_KEYDOWN && event.pressedkey == SDLK_BACKSPACE){
+                    if(!end && strlen(text)>0){
+                        text[strlen(text)-1]='\0';
                     }
                 }
+                
             }
+            SDL_RenderClear(renderTarget);
+            SDL_RenderCopy(renderTarget,texture[0],NULL,NULL);
+            SDL_RenderPresent(renderTarget);
         }
-        
+        /*for(int i=0; i<dim; i++){
+            SDL_FreeSurface(images[i]);
+            images[i]=NULL;
+        }*/
         SDL_DestroyWindow(window);
+        SDL_DestroyTexture(texture[0]);
+        SDL_DestroyRenderer(renderTarget);
         SDL_Quit();
     }
-}
+} 
