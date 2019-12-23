6bfc31db3624a8fbbaabf57a33b84e990ad4619a
ermiry <erick_salas@ermiry.com>
2019-12-22 23:15:07 -0600

    Refactor buffer source

--- src/app/videos/buffers/buffer.c
+++ src/app/videos/buffers/buffer.c
@@ -19,87 +19,114 @@ Buffer *buffer_create (unsigned int size, BufferFreeCallback free_cb) {
 }
 
 unsigned int buffer_length (const Buffer *buffer) {
-    return buffer->write_p - buffer->read_p;
+
+    if (buffer) return buffer->write_p - buffer->read_p;
+    else return 0;
+
 }
 
 void buffer_destroy (Buffer *buffer) {
-    if(buffer == NULL) return;
-    buffer_clear(buffer);
-    free(buffer->data);
-    free(buffer);
+    
+    if (buffer) {
+        buffer_clear (buffer);
+        free (buffer->data);
+        free (buffer);
+    }
+
 }
 
 void buffer_clear (Buffer *buffer) {
-    void *data;
-    if(buffer->free_cb == NULL)
-        return;
-    while((data = buffer_read (buffer)) != NULL) {
-        buffer->free_cb(data);
+
+    if (buffer) {
+        void *data;
+        if(buffer->free_cb == NULL)
+            return;
+        while((data = buffer_read (buffer)) != NULL) {
+            buffer->free_cb(data);
+        }
     }
+    
 }
 
 void *buffer_read (Buffer *buffer) {
-    assert(buffer != NULL);
-    if(buffer->read_p < buffer->write_p) {
-        void *out = buffer->data[buffer->read_p % buffer->size];
-        buffer->data[buffer->read_p % buffer->size] = NULL;
-        buffer->read_p++;
-        if(buffer->read_p >= buffer->size) {
-            buffer->read_p = buffer->read_p % buffer->size;
-            buffer->write_p = buffer->write_p % buffer->size;
+
+    if (buffer) {
+        if(buffer->read_p < buffer->write_p) {
+            void *out = buffer->data[buffer->read_p % buffer->size];
+            buffer->data[buffer->read_p % buffer->size] = NULL;
+            buffer->read_p++;
+            if(buffer->read_p >= buffer->size) {
+                buffer->read_p = buffer->read_p % buffer->size;
+                buffer->write_p = buffer->write_p % buffer->size;
+            }
+            return out;
         }
-        return out;
     }
+    
     return NULL;
 }
 
 void *buffer_peek (const Buffer *buffer) {
-    assert(buffer != NULL);
-    if(buffer->read_p < buffer->write_p) {
-        return buffer->data[buffer->read_p % buffer->size];
+
+    if (buffer) {
+        if(buffer->read_p < buffer->write_p) {
+            return buffer->data[buffer->read_p % buffer->size];
+        }
     }
+    
     return NULL;
 }
 
 void buffer_advance (Buffer *buffer) {
-    assert(buffer != NULL);
-    if(buffer->read_p < buffer->write_p) {
-        buffer->data[buffer->read_p % buffer->size] = NULL;
-        buffer->read_p++;
-        if(buffer->read_p >= buffer->size) {
-            buffer->read_p = buffer->read_p % buffer->size;
-            buffer->write_p = buffer->write_p % buffer->size;
+
+    if (buffer) {
+        if(buffer->read_p < buffer->write_p) {
+            buffer->data[buffer->read_p % buffer->size] = NULL;
+            buffer->read_p++;
+            if(buffer->read_p >= buffer->size) {
+                buffer->read_p = buffer->read_p % buffer->size;
+                buffer->write_p = buffer->write_p % buffer->size;
+            }
         }
     }
+    
 }
 
 void buffer_for_each_item (const Buffer *buffer, ForEachItemCallback cb, void *userdata) {
-    unsigned int read_p = buffer->read_p;
-    unsigned int write_p = buffer->write_p;
-    while(read_p < write_p) {
-        cb(buffer->data[read_p++ % buffer->size], userdata);
-        if(read_p >= buffer->size) {
-            read_p = read_p % buffer->size;
-            write_p = write_p % buffer->size;
+
+    if (buffer) {
+        unsigned int read_p = buffer->read_p;
+        unsigned int write_p = buffer->write_p;
+        while(read_p < write_p) {
+            cb(buffer->data[read_p++ % buffer->size], userdata);
+            if(read_p >= buffer->size) {
+                read_p = read_p % buffer->size;
+                write_p = write_p % buffer->size;
+            }
         }
     }
+    
 }
 
 int buffer_write (Buffer *buffer, void *ptr) {
-    assert(buffer != NULL);
-    assert(ptr != NULL);
 
-    if(!buffer_is_full (buffer)) {
-        buffer->data[buffer->write_p % buffer->size] = ptr;
-        buffer->write_p++;
-        return 0;
+    if (buffer && ptr) {
+         if(!buffer_is_full (buffer)) {
+            buffer->data[buffer->write_p % buffer->size] = ptr;
+            buffer->write_p++;
+            return 0;
+        }
+        return 1;
     }
-    return 1;
+   
 }
 
 int buffer_is_full (const Buffer *buffer) {
 
-    int len = buffer->write_p - buffer->read_p;
-    int k = (len >= buffer->size);
-    return k;
+    if (buffer) {
+        int len = buffer->write_p - buffer->read_p;
+        int k = (len >= buffer->size);
+        return k;
+    }
+
 }
