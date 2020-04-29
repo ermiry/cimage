38084c49f0390d9ff8906af50ee8c911a17682ac
ermiry <erick_salas@ermiry.com>
2020-04-29 12:46:30 -0500

    Temporarily removed videos module for a later version

--- include/app/videos/video.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CIMAGE_MEDIA_VIDEOS_VIDEO_H_
-#define _CIMAGE_MEDIA_VIDEOS_VIDEO_H_
-
-#include "app/videos/decoder.h"
-#include "app/videos/source.h"
-
-extern Decoder *video_create_decoder (const VideoSource *src, int stream_index);
-
-#endif
--- src/app/videos/video.c
+++
@@ -1,8 +0,0 @@
-#include "app/videos/decoder.h"
-#include "app/videos/source.h"
-
-Decoder *video_create_decoder (const VideoSource *src, int stream_index) {
-
-
-
-}
