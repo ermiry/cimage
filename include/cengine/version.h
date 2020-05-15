20a64d786a8528c2b4f3f9154f1be61da9202e2e
ermiry <erick_salas@ermiry.com>
2020-05-14 19:50:28 -0500

    Refactor cengine version source

--- include/cengine/version.h
+++ include/cengine/version.h
@@ -1,11 +1,11 @@
 #ifndef _CENGINE_VERSION_H_
 #define _CENGINE_VERSION_H_
 
-#define VERSION                 "0.9.4"
-#define VERSION_NAME            "Pre-Release 0.9.4"
-#define VERSION_DATE			"07/05/2020"
-#define VERSION_TIME			"19:20 CST"
-#define VERSION_AUTHOR			"Erick Salas"
+#define CENGINE_VERSION                 "0.9.5"
+#define CENGINE_VERSION_NAME            "Pre-Release 0.9.5"
+#define CENGINE_VERSION_DATE			"14/05/2020"
+#define CENGINE_VERSION_TIME			"19:50 CST"
+#define CENGINE_VERSION_AUTHOR			"Erick Salas"
 
 // print full cengine version information 
 extern void cengine_version_print_full (void);
--- src/cengine/version.c
+++ src/cengine/version.c
@@ -5,22 +5,22 @@
 // print full cengine version information 
 void cengine_version_print_full (void) {
 
-    printf ("\n\nCengine Version: %s\n", VERSION_NAME);
-    printf ("Release Date & time: %s - %s\n", VERSION_DATE, VERSION_TIME);
-    printf ("Author: %s\n\n", VERSION_AUTHOR);
+    printf ("\nCengine Version: %s\n", CENGINE_VERSION_NAME);
+    printf ("Release Date & time: %s - %s\n", CENGINE_VERSION_DATE, CENGINE_VERSION_TIME);
+    printf ("Author: %s\n\n", CENGINE_VERSION_AUTHOR);
 
 }
 
 // print the version id
 void cengine_version_print_version_id (void) {
 
-    printf ("\n\nCengine Version ID: %s\n", VERSION);
+    printf ("\n\nCengine Version ID: %s\n", CENGINE_VERSION);
 
 }
 
 // print the version name
 void cengine_version_print_version_name (void) {
 
-    printf ("\n\nCengine Version: %s\n", VERSION_NAME);
+    printf ("\n\nCengine Version: %s\n", CENGINE_VERSION_NAME);
 
 }
